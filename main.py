9a46c15499b6256b40e90988c61f96c3de9511e3
spolli <kebabnopiccante@gmail.com>
2019-11-09 15:41:55 +0100

    it works

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, jsonify, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect
 from src.models.animeforce import Enforcer, download_link, Anime
-import json
 
 app = Flask(__name__, template_folder='./src/templates')
-loading = True
+video_link = None
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+    loading = True
     if request.method == 'POST':
+        global video_link
         date = request.get_json()
         anime = Anime(date['name'], date['link'], date['episodeNumber'], date['type'], date['img'])
         video_link = anime._get_last_episode().download_link()
-        return redirect(url_for('video.html', video_link=video_link))
     else:
-        global loading
         enforcer = Enforcer()
         lastEpisodeList = enforcer.last_episode_list()
         loading = False
-        return render_template('index.html', lastEpisodeList=lastEpisodeList)
+    return render_template('index.html', lastEpisodeList=lastEpisodeList, loading=loading)
+
+@app.route("/video/")
+def video_page():
+    return render_template('video.html', video_link=video_link)
 
 @app.route("/animelist")
 def fullAnimeList():
